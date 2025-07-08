041d1f7969af80609caef73c29b8c66f14c1210d
Dipesh <dipeshtharu444@gmail.com>
2025-07-08 20:34:03 +0545

    #add new file

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid gap-5 mx-5">
     <a class="navbar-brand text-primary fw-bold" href="">RoomFinder</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include_once __DIR__ . '/includes/header.php'; ?>
 <?php include_once __DIR__ . '/includes/navbar.php'; ?>
 
-<section class="hero-section bg-light py-5">
+<section class="hero-section py-5">
     <div class="container">
         <div class="row justify-content-center align-items-center">
             <div class="col-lg-8">
@@ -13,19 +13,39 @@
                         <input type="text" class="form-control" placeholder="Location (e.g.,Kathmandu)">
                     </div>
                     <div class="col-md-3">
-                            <select name="room-type" id="room-type" class="form-control">
-                                <option value="">Select Room Type</option>
-                                <option value="single_room">Single Room</option>
-                                <option value="flat">Flat</option>
-                                <option value="shared_room">Shared Room</option>
-                                <option value="double_room">Double Room</option>
-                            </select>
+                        <select name="room-type" id="room-type" class="form-control">
+                            <option value="">Select Room Type</option>
+                            <option value="single_room">Single Room</option>
+                            <option value="flat">Flat</option>
+                            <option value="shared_room">Shared Room</option>
+                            <option value="double_room">Double Room</option>
+                        </select>
                     </div>
                     <div class="col-md-2">
                         <button class="btn btn-primary">Search</button>
                     </div>
                 </form>
             </div>
+        </div>
+    </div>
+
+</section>
+<section class="room-listing py-5">
+    <div class="container">
+        <h2 class="text-center text-primary mb-4">Available Rooms</h2>
+        <div class="row g-4">
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card’s content.</p>
+                        <a href="#" class="btn btn-primary">Details</a>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 
